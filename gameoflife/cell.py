8032ce6efb0ef4470eab0ae8f034c942e88a136c
Cole <cebCole@gmail.com>
2021-02-26 10:39:35 -0600

    sandbox testing changes for controls

--- gameoflife/cell.py
+++ gameoflife/cell.py
@@ -5,7 +5,8 @@ from pyVariables import *
 
 class Cell:
     """
-    A class to represent a single cell for Conway's Game of Life
+    A class to represent a single cell, created using pygame.Rect object,
+        designed for use in Conway's Game of Life program.
     """
     def __init__(self, rect, state=0):
         self.rect = pygame.Rect(rect)
@@ -43,8 +44,7 @@ class Cell:
 
 if __name__ == '__main__':
     pygame.init()
-    WIDTH = 600
-    surface = pygame.display.set_mode((WIDTH, WIDTH))
+    surface = pygame.display.set_mode((DIS_X, DIS_Y))
     surface.fill((BG_COLOR))
     pygame.display.set_caption("Conway's Game of Life Cell")
 
@@ -57,15 +57,34 @@ if __name__ == '__main__':
             row.append(Cell(rect=(100+i*30,100+j*30,28,28), state=0))
         cells.append(row)
 
+
+    def get_cell(pos):
+        i = (pos[0] - 100) // 30
+        j = (pos[1] - 100) // 30
+        return i, j
+
+    def collision_check(i,j):
+        if i >=0 and i <= 9  and j >=0 and j <=9:
+            return True
+        return False
+
+
     def clickChange(pos):
         """Mouse control option:
         Controls state of cell by first finding the i,j value of the cell. Then
         calling the change state method on said cell if called. """
-        i = (pos[0] - 100) // 30
-        j = (pos[1] - 100)// 30
-        print(f"i:{i}----j:{j}----pos:{pos}")
-        if i >=0 and i <= 9  and j >=0 and j <=9:
+        i, j = get_cell(pos)
+        if collision_check(i,j):
             cells[i][j].change_state()
+
+    last_cell_change = None
+    def motionChange(pos):
+        global last_cell_change
+        i, j = get_cell(pos)
+        print(f"i:{i}----j:{j}----pos:{pos}")
+        if collision_check(i,j) == True and last_cell_change != cells[i][j]:
+            cells[i][j].change_state()
+            last_cell_change = cells[i][j]
 
 
     print(cells)
@@ -82,7 +101,7 @@ if __name__ == '__main__':
                     clickChange(pygame.mouse.get_pos())
             if event.type == pygame.MOUSEMOTION:
                 if pygame.mouse.get_pressed()[0]:
-                    clickChange(pygame.mouse.get_pos())
+                    motionChange(pygame.mouse.get_pos())
 
 
         surface.fill((BG_COLOR))
