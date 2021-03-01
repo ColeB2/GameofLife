91872e1813f448e5967f2905b050ab9c71f3a81e
Cole <cebCole@gmail.com>
2021-03-01 09:42:03 -0600

    working on board changes to fix logic

--- gameoflife/board.py
+++ gameoflife/board.py
@@ -1,4 +1,3 @@
-import pygame
 from pyVariables import *
 from cell import Cell
 
@@ -6,44 +5,112 @@ from cell import Cell
 class Board:
     """
     A class to represent a group of cells, laid out in a board like fashion,
-        designed for use in Conway's Game of Life.
+    designed for use in Conway's Game of Life.
     """
     def __init__(self, width=10, height=10):
         self.width = width #row
         self.height = height #col
         self.board = []
-        self.all_cells = []
         self.create_board()
+        print(self.board)
+        self.set_cell_neighbours()
+
+
+    def print_board(self):
+        print(self.board)
+        # for i in range(len(self.board)):
+            # print(self.board[i])
 
 
 
     def create_board(self):
-        for i in range(self.height):
+        for i in range(self.width):
             row = []
-            for j in range(self.width):
-                row.append(Cell(rect=(100+i*30, 100+j*30, 28,28), state=0))
+            for j in range(self.height):
+                row.append(Cell(x=i,
+                                y=j,
+                                state=0))
             self.board.append(row)
+
+
+    def dead_state(self):
+        """Kills all cells in the board, does so by iterating through all cells
+        and changing their state to 0."""
+        for row in range(len(self.board)):
+            for cell in self.board[row]:
+                cell.state = 0
+
+
+    def set_cell_neighbours(self):
+        """Iterates through all cells, and creates a list of neighbours
+        for each cell."""
+        for row in range(len(self.board)):
+            for cell in self.board[row]:
+                cell.get_neighbours(self.board)
+
+
+
+    def next_state(self):
+        """
+        Calculates the next state the board should be in. Does so by
+        iterating through all cells in the board, and calling their
+        calculate_state methods
+        """
+        for row in range(len(self.board)):
+            for cell in self.board[row]:
+                cell.calculate_state()
 
 
 
 if __name__ == "__main__":
+    from buttons import Button
+    import pygame
     pygame.init()
     surface = pygame.display.set_mode((DIS_X, DIS_Y))
     surface.fill((BG_COLOR))
     pygame.display.set_caption("Conway's Game of Life Board")
 
-    B = Board(width=10, height=10)
+    """button"""
+    UPDATE = False
+    def BF():
+        global UPDATE
+        if UPDATE == False:
+            UPDATE = True
+        else:
+            UPDATE = False
+
+    BB = Button(rect=(0,0,100,100), function=BF)
+
+    B = Board(width=BOARD_WIDTH, height=BOARD_HEIGHT)
+
+
+    def get_cell(pos):
+        i = (pos[0] - TOP_LEFT_X) // CELL_WIDTH
+        j = (pos[1] - TOP_LEFT_Y) // CELL_WIDTH
+        return i, j
+
+    def collision_check(i,j):
+        if i >=0 and i <= BOARD_WIDTH-1  and j >=0 and j <=BOARD_HEIGHT-1:
+            return True
+        return False
 
 
     def clickChange(pos):
         """Mouse control option:
         Controls state of cell by first finding the i,j value of the cell. Then
         calling the change state method on said cell if called. """
-        i = (pos[0] - 100) // 30
-        j = (pos[1] - 100)// 30
-        print(f"i:{i}----j:{j}----pos:{pos}")
-        if i >=0 and i <= 9  and j >=0 and j <=9:
+        i, j = get_cell(pos)
+        if collision_check(i,j):
             B.board[i][j].change_state()
+
+    last_cell_change = None
+    def motionChange(pos):
+        global last_cell_change
+        i, j = get_cell(pos)
+        # print(f"i:{i}----j:{j}----pos:{pos}")
+        if collision_check(i,j) == True and last_cell_change != B.board[i][j]:
+            B.board[i][j].change_state()
+            last_cell_change = B.board[i][j]
 
     run = True
     """Main Loop"""
@@ -57,11 +124,19 @@ if __name__ == "__main__":
                     clickChange(pygame.mouse.get_pos())
             if event.type == pygame.MOUSEMOTION:
                 if pygame.mouse.get_pressed()[0]:
-                    clickChange(pygame.mouse.get_pos())
+                    motionChange(pygame.mouse.get_pos())
+
+            BB.get_event(event)
 
 
         surface.fill((BG_COLOR))
         for row in B.board:
             for cell in row:
                 cell.update(surface)
+
+        BB.update(surface)
+        if UPDATE:
+            B.next_state()
+            pygame.time.wait(100)
+        # B.print_board()
         pygame.display.update()
