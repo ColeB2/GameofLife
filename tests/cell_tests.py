fc315fafe1d0e4f335a46b4675a14973d72e6304
Cole <cebCole@gmail.com>
2021-03-08 07:23:29 -0600

    more cell unit tests

--- tests/cell_tests.py
+++ tests/cell_tests.py
@@ -7,6 +7,9 @@ from cell import Cell
 class TestCell(unittest.TestCase):
     @classmethod
     def setUpClass(self):
+        """Basic Cell class setup. Creating a mini 4 cell neighbourhood, and
+        calling the get_neighbours class to set up neighbours on all cells.
+        Doesn't call get_neighbours on C4, as to save it for a later test."""
         self.C1 = Cell(0,0, state=0)
         self.C2 = Cell(0,1, state=0)
         self.C3 = Cell(1,0, state=0)
@@ -14,11 +17,15 @@ class TestCell(unittest.TestCase):
 
         self.Board = [[self.C1, self.C2],[self.C3, self.C4]]
         self.C1.get_neighbours(self.Board)
+        self.C2.get_neighbours(self.Board)
+        self.C3.get_neighbours(self.Board)
 
     def test_proper_setup(self):
         self.assertEqual(self.C1.x, 0)
         self.assertEqual(self.C1.y, 0)
         self.assertEqual(self.C1.state, 0)
+        self.assertEqual(self.C1.prev_state, self.C1.state)
+        self.assertEqual(len(self.C1.neighbours), 3)
 
     def test_get_neighbours(self):
         Answer = [self.C3, self.C2, self.C4]
@@ -39,6 +46,8 @@ class TestCell(unittest.TestCase):
     def test_calculate_state(self):
         self.C4.get_neighbours(self.Board)
         self.assertEqual(self.C4.state, 1)
+        self.C4.check_neighbour_state()
+        self.assertEqual(self.C4.alive_neighbours, 0)
         self.C4.calculate_state()
         self.assertEqual(self.C4.state, 0)
 
